11c3d8025127e0822eab2bc560d40f0a29089954
jawadhussieny-dot <jawadhussieny@gmail.com>
2026-03-21 20:00:01 +0200

    Update exerciseLibrary.jsx

--- src/pages/exerciseLibrary.jsx
+++ src/pages/exerciseLibrary.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import {
   getUserProfile,
   dailyCalorieLog,
