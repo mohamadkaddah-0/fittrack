abd003250fd48198beabc88c46215d58ffe17498
jawadhussieny-dot <jawadhussieny@gmail.com>
2026-03-21 19:52:44 +0200

    deleting the navBar to match with the created nav Component

--- src/pages/exerciseLibrary.jsx
+++ src/pages/exerciseLibrary.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import {
   getUserProfile,
   dailyCalorieLog,
